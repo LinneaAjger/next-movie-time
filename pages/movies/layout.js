915d3ab87c6bc2cfbd47065a6fb523d3205390d0
LinneaAjger <linnea.ajger@gmail.com>
2023-03-29 19:22:31 +0200

    created more universal pages for genre and movie details

--- pages/movies/layout.js
+++ pages/movies/layout.js
@@ -1,22 +1,22 @@
 import Link from "next/link"
 
 export default function RootLayout({ children }) {
-  const links = [
-    {label: 'Popular', path: '/movies/popular', targetSegment: 'popular'}, 
-    {label: 'Top-rated', path: '/movies/top_rated', targetSegment: 'top-rated'},
-    {label: 'Upcoming', path: '/movies/upcoming', targetSegment: 'upcoming'}
-  ]
+  // const links = [
+  //   {label: 'Popular', path: '/movies/popular', targetSegment: 'popular'}, 
+  //   {label: 'Top-rated', path: '/movies/top_rated', targetSegment: 'top-rated'},
+  //   {label: 'Upcoming', path: '/movies/upcoming', targetSegment: 'upcoming'}
+  // ]
   
 return (
     <>
-        <ul className="second-nav">        
+        {/* <ul className="second-nav">        
           {links.map ( (l, i) => 
           <li key={i}>
             <Link key={i} {...l} />
           </li>
         )}
         </ul>
-        {children}
+        {children} */}
       </>
       )
 }
