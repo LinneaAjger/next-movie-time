b36590db83b6acfd6f447e83c9105b8bc1e6736f
LinneaAjger <linnea.ajger@gmail.com>
2025-02-17 20:44:34 +0100

    new thumbnail added

--- components/Nav.js
+++ components/Nav.js
@@ -2,16 +2,32 @@ import Link from 'next/link'
 import navStyles from '@/styles/Nav.module.css'
 
 const Nav = () => {
+  
+  const links = [
+    {label: 'Popular', path: '/movies/popular', targetSegment: 'popular'}, 
+    {label: 'Top-rated', path: '/movies/top_rated', targetSegment: 'top-rated'},
+    {label: 'Upcoming', path: '/movies/upcoming', targetSegment: 'upcoming'}
+  ]
+
   return (
     <nav className={navStyles.nav}>
-      <ul>
+              <ul className="second-nav">        
+          {links.map ( (link, i) => 
+          <li key={i}>
+            <Link href={`${link.path}`}> 
+                            {link.label} 
+                        </Link> 
+          </li>
+        )}
+        </ul>
+      {/* <ul>
         <li>
           <Link href='/'>Home</Link>
         </li>
         <li>
           <Link href='/about'>About</Link>
         </li>
-      </ul>
+      </ul> */}
 
     </nav>
   )
--- pages/movies/[genre].js
+++ pages/movies/[genre].js
@@ -1,29 +1,37 @@
 import MovieItem from "@/components/MovieItem";
 import styles from '@/styles/Layout.module.css'
+import Link from 'next/link'
+import { useState } from "react";
 
 
 export default function Genre({ movieRes }) {
+  const [currentPage, setCurrentPage] = useState(1)
+  console.log(currentPage)
+  console.log(movieRes)
+
   return(
-    <section className={styles.section} >
-      {movieRes.results.map((movie)=>
-      <MovieItem
-      title={movie.title}
-      id={movie.id}
-      poster_path={movie.poster_path}
-      release_date={movie.release_date}
-      vote_average={movie.vote_average}
-       />)}
-    </section>
+        <section className={styles.section} >
+          {movieRes.results.map((movie)=>
+          <MovieItem
+          title={movie.title}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          release_date={movie.release_date}
+          vote_average={movie.vote_average}
+          />)}
+          {/* <Link href={`/${genre}/${currentPage}`}>Next 20 movies</Link> */}
+        </section>
   )
 }
 
 export async function getServerSideProps(context) {
   const { genre } = context.query
   const movieData = await fetch(
-    `https://api.themoviedb.org/3/movie/${genre}?api_key=${process.env.API_KEY}`
+    `https://api.themoviedb.org/3/movie/${genre}?api_key=${process.env.API_KEY}&page=2`
   )
   const movieRes = await movieData.json();
   return {
-    props: {movieRes}
+    props: {movieRes}  
+    
   }
 }
